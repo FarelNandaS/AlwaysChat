4612a2e90ddd347a8cb8e4713d512192eeb34aa4
farel nanda setiawan <farelnanda11@gmail.com>
2026-09-23 00:29:59 +0700

    mengubah alert session expirate api menjadi alert custom

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Echo from "laravel-echo";
 import Pusher from "pusher-js";
 import { router } from "@inertiajs/vue3";
+import { alert } from "./Components/Alert.vue";
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
